6e7324a2ce219171438f0e7c244ece4e4b5afd5e
jtapolczai <janos.tapolczai+jhg78g3rfvofkh@gmail.com>
2014-05-03 13:25:31 +0200

    Added MonadPlus' class to Monad.
    Added the MonadPlus' class to Monad.MultiParam.
    
    Other changes:
    
    * added the operators =<<, >=>, <=<.
    * added the functions of Control.Monad as undefined stubs.

--- Control/Monad/MultiParam.hs
+++ Control/Monad/MultiParam.hs
@@ -19,10 +19,14 @@
 --  Adapted from 'http://okmij.org/ftp/Haskell/types.html#restricted-datatypes'.
 module Control.Monad.MultiParam (
    Monad'(..),
+   MonadPlus'(..),
+   (=<<),
+   (>=>),
+   (<=<),
    filterM,
    ) where
 
-import Prelude hiding ((>>=), Functor(..))
+import Prelude hiding ((>>=), (=<<), Functor(..), Monad (..))
 import qualified Control.Monad as Mo
 import qualified Data.Set as Set
 import Data.Functor.MultiParam
@@ -48,8 +52,22 @@ class (Pure m a, Applicative' m a b) => Monad' m a b where
    -- |Flattens the monadic structure.
    join m = m >>= id
 
+-- |Synonym for @pure@.
+return :: (Pure m a) => a -> m a
+return = pure
+
+-- |MonadPlus class with type parameter for the contained type.
+--  '(mzero,mplus)' form a monoid over the monad 'm a'.
+class (Monad' m a a) => MonadPlus' m a where
+   mzero :: m a
+   mplus :: m a -> m a -> m a
+
 instance Monad' [] a b where
    x >>= f = x Mo.>>= f
+
+instance MonadPlus' [] a where
+   mzero = Mo.mzero
+   mplus = Mo.mplus
 
 instance Monad' IO a b where
    x >>= f = x Mo.>>= f
@@ -57,17 +75,40 @@ instance Monad' IO a b where
 instance Monad' Maybe a b where
    x >>= f = x Mo.>>= f
 
+instance MonadPlus' Maybe a where
+   mzero = Mo.mzero
+   mplus = Mo.mplus
+
 instance Monad' ReadP a b where
    x >>= f = x Mo.>>= f
+
+instance MonadPlus' ReadP a where
+   mzero = Mo.mzero
+   mplus = Mo.mplus
 
 instance Monad' ReadPrec a b where
    x >>= f = x Mo.>>= f
 
+instance MonadPlus' ReadPrec a where
+   mzero = Mo.mzero
+   mplus = Mo.mplus
+
 instance Monad' STM a b where
    x >>= f = x Mo.>>= f
 
+instance MonadPlus' STM a where
+   mzero = Mo.mzero
+   mplus = Mo.mplus
+
 instance (Ord a, Ord b) => Monad' Set.Set a b where
    x >>= f = Set.foldl' Set.union Set.empty $ Set.map f x
+
+instance Ord a => MonadPlus' Set.Set a where
+   mzero = Set.empty
+   mplus = Set.union
+
+
+-- These functions are basically copies of those found in @Control.Monad@.
 
 filterM :: (Monad' m Bool [a], Monad' m [a] [a]) => (a -> m Bool) -> [a] -> m [a]
 filterM _ [] = pure []
@@ -75,3 +116,41 @@ filterM f (x:xs) = f x
                    >>= (\t -> filterM f xs
                    >>= (\ys -> pure $! if t then x:ys else ys))
 
+-- |Flipped version of @>>=@.
+(=<<) :: Monad' m a b => (a -> m b) -> m a -> m b
+(=<<) = flip (>>=)
+
+-- |Monadic version of function concatenation @.@.
+(>=>) :: (Monad' m a b, Monad' m b c) => (a -> m b) -> (b -> m c) -> a -> m c
+(>=>) f g x = pure x >>= f >>= g 
+
+-- |Flipped version of @>=>@.
+(<=<) :: (Monad' m a b, Monad' m b c) => (b -> m c) -> (a -> m b) -> a -> m c
+(<=<) = flip (>=>)
+
+-- todo 
+foldM = undefined
+foldM_ = undefined
+forM = undefined
+forM_ = undefined
+forever = undefined
+guard = undefined
+liftM = undefined
+liftM2 = undefined
+liftM3 = undefined
+liftM4 = undefined
+liftM5 = undefined
+mapAndUnzipM = undefined
+mapM = undefined
+mapM_ = undefined
+mfilter = undefined
+msum = undefined
+replicateM = undefined
+replicateM_ = undefined
+sequence = undefined
+sequence_ = undefined
+unless = undefined
+void = undefined
+when = undefined
+zipWithM = undefined
+zipWithM_ = undefined
